1d21bc90abfc31a06ff211fdee0c27afc0e16a00
Rahul Ghosal <rghosal@Rahuls-MacBook-Pro.local>
2023-02-25 13:51:28 -0500

    remove mongodb client factory, prettier formatting

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,21 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { MongoClient } from 'mongodb'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
-
-async function getClient(): Promise<MongoClient> {
-  const client: MongoClient = await new MongoClient(process.env.MONGO_URI as string).connect();
-  await client.db('test').collection('test').insertOne({message: 'inserted from test'});
-  return client;
-}
-
+  name: string;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: "John Doe" });
 }
