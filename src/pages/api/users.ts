db56f66055ffeee1f6ef2aa149ecd7d1db4a23fb
Rahul Ghosal <rdghosal@gmail.com>
2023-02-26 05:32:48 -0500

    fix unresolved API response and unhandled promise rejection at mongo error

--- src/pages/api/users.ts
+++ src/pages/api/users.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import getMongoClient from "@/clients/mongodb";
 import User from "@/models/user";
+import { resolve } from "path";
 
 type Data = {
   name: string;
@@ -14,13 +15,15 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
   if (req.method === "POST") {
-    await addUser(req);
+    await addUser(req)
+      .then(() => res.status(200).end())
+      .catch((error) => res.status(500).end());
   }
-  res.status(200);
 }
 
 async function addUser(req: NextApiRequest): Promise<void> {
   const user: User = req.body;
   console.log(user);
-  (await getMongoClient()).db(DATABASE).collection(COLLECTION).insertOne(user);
+  const client = await getMongoClient();
+  await client.db(DATABASE).collection(COLLECTION).insertOne(user);
 }
