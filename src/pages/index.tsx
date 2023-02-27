ae040feebd9cea7bf818ecc2f95ea13c7c3e5514
Rahul Ghosal <rdghosal@gmail.com>
2023-02-26 20:46:48 -0500

    add landing page css and login route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,26 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Landing.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
-  return <button onClick={() => fetchHello()} type='button'>Click Me!</button>
-}
-
-
-async function fetchHello() {
-  const r = await fetch('/api/hello');
-  console.log(await r.json());
+export default function Landing() {
+  return (
+    <div className={styles.main}>
+      <div className={styles.overlay}>
+        <p className={styles.title}>buckit.</p>
+        <p className={styles.text}>
+          Lorem, ipsum dolor. Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit.
+        </p>
+        <button type="button" className={styles.registerBtn}>
+          sign up
+        </button>
+        <button type="button" className={styles.signInBtn}>
+          sign in
+        </button>
+      </div>
+    </div>
+  );
 }
