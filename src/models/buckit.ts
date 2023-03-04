44e7a45288bc67decfbd6b4097817d3c3feb28f2
Rahul Ghosal <rdghosal@gmail.com>
2023-03-04 07:39:22 -0500

    refactor models, add yup schemas

--- src/models/buckit.ts
+++ src/models/buckit.ts
@@ -1,47 +1,96 @@
 import BaseRecord from "./basemodel";
+import * as yup from "yup";
 
-class Buckit extends BaseRecord {
+// ----------------------------------------------------------------- < Base >
+
+class BaseInput extends BaseRecord {
   title: string;
+  createdBy: string | null;
   description: string | null;
-  userIds: string[] = [];
-  drops: Drop[] = [];
+
   constructor(
     title: string,
-    description: string,
-    drops: Drop[] = [],
-    ...p: ConstructorParameters<typeof BaseRecord>
+    createdBy?: string,
+    description?: string,
+    ...args: ConstructorParameters<typeof BaseRecord>
   ) {
-    super(...p);
+    super(...args);
     this.title = title;
-    this.description = description;
-    this.drops = drops;
+    this.createdBy = createdBy || null;
+    this.description = description || null;
   }
 }
 
-class Drop extends BaseRecord {
+interface IBaseInput {
+  _id?: string;
   title: string;
-  description: string | null;
-  location: string;
-  status: DropStatus;
+  createdBy?: string;
+  description?: string;
+}
+
+// --------------------------------------------------------------- < Buckets >
+
+export class Bucket extends BaseInput {
+  userIds: string[];
   constructor(
-    title: string,
-    description: string,
-    location: string,
-    status: DropStatus,
-    ...p: ConstructorParameters<typeof BaseRecord>
+    userIds: string[] = [],
+    ...args: ConstructorParameters<typeof BaseInput>
   ) {
-    super(...p);
-    this.title = title;
-    this.description = description;
-    this.location = location;
-    this.status = status;
+    super(...args);
+    this.userIds = userIds;
   }
 }
+
+export interface IBucket extends IBaseInput {
+  userIds?: string[];
+}
+
+export const bucketSchema = new yup.ObjectSchema<IBucket>({
+  _id: yup.string().optional(),
+  title: yup.string().required(),
+  createdBy: yup.string().optional(),
+  description: yup.string().optional(),
+  userIds: yup.array(yup.string().required()).optional(),
+});
+
+// ----------------------------------------------------------------  < Drops >
 
 enum DropStatus {
   DONE = "done",
   PAUSED = "paused",
-  PROGRESS = "progress",
+  ACTIVE = "active",
 }
 
-export { Buckit, Drop, DropStatus };
+export class Drop extends BaseInput {
+  dueDate: string | null;
+  timeCompleted: string | null;
+  location: string | null;
+  status: DropStatus;
+  constructor(
+    dueDate?: string,
+    timeCompleted?: string,
+    location?: string,
+    status?: DropStatus,
+    ...args: ConstructorParameters<typeof BaseInput>
+  ) {
+    super(...args);
+    this.dueDate = dueDate || null;
+    this.timeCompleted = timeCompleted || null;
+    this.location = location || null;
+    this.status = status || DropStatus.ACTIVE;
+  }
+}
+
+export interface IDrop extends IBaseInput {
+  dueDate?: string;
+  location?: string;
+}
+
+export const dropSchema = new yup.ObjectSchema<IDrop>({
+  _id: yup.string().optional(),
+  title: yup.string().required(),
+  createdBy: yup.string().optional(),
+  description: yup.string().optional(),
+  location: yup.string().optional(),
+  dueDate: yup.string().optional(),
+});
