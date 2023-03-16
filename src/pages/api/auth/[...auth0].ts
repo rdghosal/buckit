3d577d61224351d7f40130c1beb852a5b744283e
Rahul Ghosal <rdghosal@gmail.com>
2023-03-16 07:28:28 -0400

    feat: register auth0 user in mongodb at callback upon successful login

--- src/pages/api/auth/[...auth0].ts
+++ src/pages/api/auth/[...auth0].ts
@@ -1,3 +1,51 @@
-import { handleAuth } from "@auth0/nextjs-auth0";
+import getMongoClient from "@/clients/mongodb";
+import { IUser, User, userSchema } from "@/models/user";
+import { handleAuth, handleCallback, Session } from "@auth0/nextjs-auth0";
+import { NextApiRequest, NextApiResponse } from "next";
 
-export default handleAuth();
+// TODO: refactor registerUser to separate file
+const DATABASE = "test";
+const COLLECTION = "users";
+
+async function registerUser(session: Session): Promise<void> {
+  try {
+    const user: IUser = await userSchema.validate(
+        { name: session.user.name, email: session.user.email });
+    const userModel: User = new User(user.name, user.email, user._id);
+    const client = await getMongoClient();
+    await client.db(DATABASE).collection(COLLECTION).updateOne(
+        { name: userModel.name, email: userModel.email, isActive: true }, 
+        { $set: userModel },
+        { upsert: true });
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+const afterCallback = async (
+    req: NextApiRequest,
+    res: NextApiResponse,
+    session: Session,
+    state
+    ) => {
+    try {
+        registerUser(session);
+    } catch(error) {
+        console.error(error);
+        throw error;
+    }
+    res.redirect("/buckets");
+    return session;
+};
+
+export default handleAuth({
+    async callback(req, res) {
+        try {
+            await handleCallback(req, res, { afterCallback });
+        } catch (error) {
+            res.status(error.status || 500).end();
+        }
+    }
+});
+
